f76fd8fcd7a1e340529ec1cc727c4828f4717598
Tyton25 <tybenz1gmail.com>
2021-01-24 22:15:40 -0700

    Adds is_leap_year.py and relocates files.

--- hackerrank/src/evenodd.py
+++ hackerrank/src/evenodd.py
@@ -8,7 +8,15 @@ import sys
 
 
 def is_odd(num):
-    if num % 2 != 0:
+    is_even = num % 2 == 0
+    if is_even:
+        if 2 <= num <= 5:
+            print("Not Weird")
+        elif 6 <= num <= 20:
+            print("Weird")
+        elif num > 20:
+            print("Not Weird")
+    else:
         print("Weird")
 
 
